20eae912b2563e840a6a67c1d2158645646c17a2
SANDQVISTT <jonathan.sandqvist@gritacademy.se>
2022-02-28 17:24:11 +0100

    lagt till knappar
    knappar på varjeplagg

--- script/display.js
+++ script/display.js
@@ -8,12 +8,15 @@ export async function itemDisplay(){
     await fetchUrl().then((data) =>{
         let array = data.products;
         let arrayLength = array.length;
+        
         for(let i = 0; i<arrayLength; i++){
             
             const divs = document.createElement('div');
+            
             divs.classList.add('products');
             
             const bodyEl = document.createElement('div')
+
             document.body.appendChild(bodyEl)
             
 
@@ -37,13 +40,76 @@ export async function itemDisplay(){
             const photo = document.createElement('img')
             photo.src = data.products[i].photo;
             divs.appendChild(photo);
-        
-            
+          
+            // knappar
+           
+            const knapp1 = document.createElement('button');
+            const knapp2 = document.createElement('button');
+            knapp1.innerText = "Add to cart";
+            knapp2.innerText = "delete from cart"
+            divs.appendChild(knapp1);
+            divs.appendChild(knapp2);
+
+
+
             bodyEl.appendChild(divs)
+            
 
             console.log(array[i])
             console.log(divs)
-
+            
         }
     })
 }
+
+
+
+
+
+
+// denna är ifrån stackoverflow men har den som inspo bara
+/* 
+class Item{
+    constructor(name,price,quantity) {
+      this.name = name;
+      this.price = price;
+      this.quantity = quantity;
+     
+    }
+  }
+  
+  class ShoppingCart {
+    constructor() {
+      this.items = []
+    }
+    
+    addItem(item){
+      this.items.push(item)
+    }
+  }
+  
+  let items = [
+    new Item("+","-",20),
+    new Item("svarta sneakers","1200kr",10),
+    new Item("Axelrämväska","399kr",15),
+    new Item("Versace Klocka","12000kr",5),
+    new Item("North Face Mössa","349kr",40)
+  ]
+  
+  let basket = new ShoppingCart()
+  
+  let market = document.getElementById("market")
+  let s = items.map((item,id)=>"<button onClick=addItem("+id+")>Add "+item.name+"</button>").join("")
+  market.innerHTML = s
+  
+  function addItem(id) {
+    basket.addItem(items[id])
+    showBasket()
+  } 
+  
+ /*  function showBasket() {
+    let cart = document.getElementById("items")
+    let s = basket.items.map((item,id)=>"<span style='margin: 5px'>"+item.name+"</span>").join("")
+    cart.innerHTML = s
+  }
+   */ 
